356d9a01ebe7bbd565e836d76489af905b0465b1
Xhoi <xhshahini22@beder.edu.al>
2025-03-24 17:29:05 +0100

    completed ex

---
+++ joy/bin/joy.dart
@@ -0,0 +1,5 @@
+import 'package:joy/joy.dart' as joy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${joy.calculate()}!');
+}
---
+++ joy/test/joy_test.dart
@@ -0,0 +1,8 @@
+import 'package:joy/joy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
